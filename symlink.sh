98e135cfdda86611d8312c42f53d60b1260c28ec
Dom H <dom@hxy.io>
2022-01-18 16:39:24 +0000

    Symlink all .nix files

--- symlink.sh
+++ symlink.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env sh
 
-ln -srfv configuration.nix /etc/nixos/configuration.nix
-ln -srfv hardware-configuration.nix /etc/nixos/hardware-configuration.nix
+find . -name \*.nix -exec basename {} \; | xargs -rtI{} sudo ln -srfv {} /etc/nixos/{}
