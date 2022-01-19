5b882dd550ddfb4d847275883732cbc631ae9f85
Dom H <dom@hxy.io>
2022-01-19 12:09:15 +0000

    Tidy up

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
@@ -11,59 +7,24 @@
       <home-manager/nixos>
     ];
 
-  # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Set your time zone.
   time.timeZone = "Europe/London";
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
   networking.useDHCP = false;
   networking.interfaces.enp0s5.useDHCP = true;
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  # };
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-  # Configure keymap in X11
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # sound.enable = false;
-  # hardware.pulseaudio.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Enable docker. Start on demand by socket activation rather than on boot.
   virtualisation.docker.enable = true;
   virtualisation.docker.enableOnBoot = false;
 
-  # Enable the fish shell system-wide.
-  # This provides vendor fish completions, etc.
+  # Enable the fish shell system-wide. This provides vendor fish completions, etc.
   programs.fish.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # Define a user account.
   users.users.dom = {
     isNormalUser = true;
     initialPassword = "hunter2";
@@ -77,8 +38,12 @@
     ];
   };
 
+  # Automatically log in after boot.
+  services.getty.autologinUser = "dom";
+
   # Configure user account with home-manager.
   home-manager.users.dom = { pkgs, ... }: {
+
     home.packages = with pkgs; [
       fishPlugins.pure
       gnumake
@@ -86,6 +51,7 @@
       # graphical
       electrum
     ];
+
     xsession.pointerCursor = {
       name = "Vanilla-DMZ";
       package = pkgs.vanilla-dmz;
@@ -111,12 +77,14 @@
         theme = "Sublime Snazzy";
       };
     };
+
     programs.git = {
       enable = true;
       userName = "Dom H";
       userEmail = "dom@hxy.io";
       delta.enable = true;
     };
+
     programs.fish = {
       enable = true;
       shellAbbrs = {
@@ -136,10 +104,12 @@
         v = "vagrant";
       };
     };
+
     programs.fzf = {
       enable = true;
       enableFishIntegration = true;
     };
+
     programs.vim = {
       enable = true;
       plugins = with pkgs.vimPlugins; [
@@ -163,6 +133,7 @@
       ];
       extraConfig = builtins.readFile ./config/vimrc;
     };
+
     programs.i3status = {
       enable = true;
       general = {
@@ -174,22 +145,23 @@
         "battery all".enable = false;
       };
     };
+
     programs.tmux = {
       enable = true;
       newSession = true; # spawn a session if trying to attach and none are running
       extraConfig = builtins.readFile ./config/tmux.conf;
     };
+
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  # System-wide packages.
   environment.systemPackages = with pkgs; [
     bat
+    bind # for dig
     curl
     htop
-    vim
     tree
-    bind # for dig
+    vim
   ];
 
   # System-wide environment variables.
@@ -200,6 +172,7 @@
   # Setup windowing environment
   services.xserver = {
     enable = true;
+
     layout = "us";
     dpi = 220;
 
@@ -225,17 +198,8 @@
         arandr
       ];
     };
+
   };
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   services.openssh = {
@@ -243,13 +207,6 @@
     passwordAuthentication = false;
     permitRootLogin = "no";
   };
-
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -259,9 +216,7 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "21.11"; # Did you read the comment?
 
-  # Convenience
+  # Convenience...
   security.sudo.wheelNeedsPassword = false;
-  services.getty.autologinUser = "dom";
 
 }
-
