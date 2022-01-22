6fd18de953c24a4f8d3adc55ebbeb55b0974b8d8
Dom H <dom@hxy.io>
2022-01-22 10:04:02 +0000

    Snazzy colour theme for `dwm`
    To match vim-snazzy: https://github.com/connorholyday/vim-snazzy

--- config/dwm-config.h
+++ config/dwm-config.h
@@ -7,15 +7,15 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "LigaSFMonoNerdFont-Medium:size=10" };
 static const char dmenufont[]       = "LigaSFMonoNerdFont:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#282a36"; /* inactive background, inactive border */
+static const char col_gray2[]       = "#606580"; /* active border */
+static const char col_gray3[]       = "#606580"; /* inactive text */
+static const char col_gray4[]       = "#eff0eb"; /* active text */
+static const char col_cyan[]        = "#282a36"; /* active background */
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray1 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_gray2 },
 };
 
 /* tagging */
