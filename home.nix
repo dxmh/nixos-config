589f78d6ba4e523f0ab1b97f797e38bcd2e60645
Dom H <dom@hxy.io>
2022-02-05 18:23:38 +0000

    Add `push` and `pull` abbreviations

--- home.nix
+++ home.nix
@@ -67,6 +67,8 @@ home-manager.users.dom = { pkgs, ... }: {
       gd = "git diff";
       gl = "git log";
       gs = "git status";
+      push = "git push";
+      pull = "git pull";
       m = "make -s";
       nrs = "sudo nixos-rebuild -I nixos-config=/home/dom/code/hxy/nixos-config/configuration.nix switch";
       ne = "vim ~/code/hxy/nixos-config/configuration.nix";
