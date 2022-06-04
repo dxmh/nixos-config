0bb707e2d14aaf3f50d87d9240734935aab872d3
Dom H <dom@hxy.io>
2022-06-04 18:00:22 +0100

    Adjust DWM's bar height
    It seems like DPI is now respected.
    
    https://dwm.suckless.org/patches/bar_height/

--- config/dwm-config.h
+++ config/dwm-config.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int user_bh            = 60;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static const int user_bh            = 30;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const char *fonts[]          = { "LigaSFMonoNerdFont-Medium:size=9" };
 static const char dmenufont[]       = "LigaSFMonoNerdFont:size=9";
 static const char col_gray1[]       = "#282a36"; /* inactive background, inactive border */
